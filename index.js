f7cce8c8b476f03f2cc27ea9b32faadaff1d04d8
David Dalton <david@chaddalton.com>
2022-06-02 18:24:01 -0400

    move over from codeacademy and add the js file;

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hello, World!")
+
+//implement time variable
+
+        //you need to implement time calculations in milliseconds 
+
+        //read up on epoch
+
+// have html and js communicate
+
+// write the organs of the function that we need to implement
