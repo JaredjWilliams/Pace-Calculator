5e1e61e6f1319a1bbc72a6270c7824960c88061e
unknown <Jared.Williams101697@gmail.com>
2022-06-13 16:24:03 -0400

    added functions and fields

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("Hello, World!")
+
 
 //implement time variable
 
@@ -14,7 +14,53 @@ console.log("Hello, World!")
 
 
 
-//download git bash
-//setup ssh key with github account (feel free to aks David for help on this one)
-//pull down repository from github'
+
 //start CODING! 
+let minutes = null
+let seconds = null
+let distance = null
+
+
+function findTotalMSeconds(minutes, seconds) {
+        const totalMSeconds = (minutes * 60000) + (seconds * 1000)
+        return totalMSeconds
+}
+
+
+function findMinString(totalMSeconds, distance) {
+        const mSecPer = (totalMSeconds / distance)
+        let min =  (mSecPer / 60000)
+        minString = min.toString()
+        minString = minString[0]
+        return minString 
+} 
+
+
+function findSecString(totalMSeconds, distance) {
+        const mSecPer = (totalMSeconds / distance)
+        let pace =  (mSecPer / 60000)
+        pace = pace.toString()
+        pace = pace.split('.')
+        pace = pace[1]
+        pace = '.' + pace
+        pace = parseFloat(pace)
+        pace = pace.toFixed([2])
+        pace = pace * 60
+        return pace
+}
+
+
+function calculate() {
+        minutes = document.getElementById("minutes").value;
+        seconds = document.getElementById("seconds").value;
+        distance = document.getElementById("distance").value;
+        let totalMSeconds = findTotalMSeconds(minutes, seconds);
+        let x = findMinString(totalMSeconds, distance) + ':' + findSecString(totalMSeconds, distance);
+        document.getElementById("pace").innerHTML = x;
+console.log(distance)
+}
+
+
+
+
+
